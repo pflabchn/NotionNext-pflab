6649762c216e004181e6d1588639a5c01769fbfe
pflabchn <dsjt1025@hotmail.com>
2025-01-29 08:11:13 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'falangong.cn',
   HEO_HERO_TITLE_4: '经验分享',
   HEO_HERO_TITLE_5: '只学芳疗是无根之木',
-  HEO_HERO_TITLE_LINK: 'https://mp.weixin.qq.com/s/mY8lk8qsIL8E7x1ksfed2w',
+  HEO_HERO_TITLE_LINK: 'https://falangong.cn/article/2500',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://falangong.cn/article/2500',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.falangong.cn',
+  HEO_INFO_CARD_URL3: 'https://falangong.cn/article/2500',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://falangong.cn/article/2500',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
