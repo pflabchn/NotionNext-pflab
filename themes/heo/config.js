97c83fcdd522a68fb77af445dbb84b214fc3a508
pflabchn <dsjt1025@hotmail.com>
2025-02-01 21:37:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,8 +29,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '芳疗方案', url: '/tag/芳疗方案' },
-  HEO_HERO_CATEGORY_3: { title: '单方精油', url: '/tag/单方精油' },
+  HEO_HERO_CATEGORY_2: { title: '单方精油', url: '/tag/单方精油' },
+  HEO_HERO_CATEGORY_3: { title: '芳疗方案', url: '/tag/芳疗方案' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
