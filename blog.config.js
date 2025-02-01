331dcb85d9a12133c839030d8860461e4ea526db
pflabchn <dsjt1025@hotmail.com>
2025-02-01 23:19:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '福兰贡芳香实验室', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '世界顶级芳香医学实验室', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '学芳疗先找福兰贡', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://falangong.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '芳香疗法,法兰贡,福兰贡,芳疗,精油,芳疗师,国际芳疗师,芳疗培训,IFA芳疗师,NAHA芳疗师,学习芳疗,', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
