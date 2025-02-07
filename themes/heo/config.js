61d2fe49f9bcedc169fb88168047bbf6dd496fe9
pflabchn <dsjt1025@hotmail.com>
2025-02-07 11:23:04 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -77,10 +77,10 @@ const CONFIG = {
     {
       title_1: '安娜',
       img_1: '/images/heo/安娜.webp',
-      color_1: '#ffffff',
+      color_1: '#d5ba9f',
       title_2: '法兰贡',
       img_2: '/images/heo/法兰贡1.webp',
-      color_2: '#ffffff'
+      color_2: '#fbcd08'
     },
     {
       title_1: '伊莎贝拉',
